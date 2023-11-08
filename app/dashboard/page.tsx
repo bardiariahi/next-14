c0e393b894f9f27a8f0c54120daa9300a4b93689
Bardia Riahi <riahi.b@pdn.co>
2023-11-08 09:21:26 +0330

    Add: project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+   <div className="a">
+    Dashboard page
+   </div>
+  );
+}
